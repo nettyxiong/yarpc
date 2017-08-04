6d2304744b4653da77ada6640630f1ff6028ce91
sxiong <sxiong@hust.edu.cn>
2017-08-04 22:43:05 +0800

    delete all

--- yarpc-common/src/main/java/org/sxiong/yarpc/common/registry/ProviderStateListener.java
+++
@@ -1,8 +0,0 @@
-package org.sxiong.yarpc.common.registry;
-
-/**
- * Created by sxiong on 7/29/17.
- */
-public interface ProviderStateListener {
-    void onProviderChange(String serviceName);
-}
--- yarpc-demo/src/main/java/org/sxiong/yarpc/demo/api/DemoService.java
+++
@@ -1,9 +0,0 @@
-package org.sxiong.yarpc.demo.api;
-
-/**
- * Created by sxiong on 7/29/17.
- */
-public interface DemoService {
-    String echo(String input);
-    MessageDTO loadObject(int messageId);
-}
